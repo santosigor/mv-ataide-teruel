873e2ccfaffb49841abc76fee7580c43b9884e0b
Igor dos Santos <igor.santos@fiap.com.br>
2018-07-10 12:02:19 -0300

    dev

--- index.php
+++ index.php
@@ -15,8 +15,10 @@
       <div class="container text-center">
         <div class="inicio">
 
+					<img src="images/ataide.jpg" alt="">
+
           <h1 class="text-uppercase text-white">1000 já apoiaram</h1>
-          <h3 class="text-uppercase text-white">Apoie você também, <span class="p-2 bg-info"><i>Ataíde Teruel (o novo)</i></span></h3>
+          <h3 class="text-uppercase text-white">Apoie você também, <span class="p-2 bg-info">Ataíde Teruel [o novo]</span></h3>
 
           <form class="form-inline">
             <input type="text" class="form-control mb-2 mr-sm-2" id="seunome" placeholder="Seu Nome">
@@ -26,9 +28,85 @@
 
         </div>
       </div>
+      <div id="scroll" class="text-center"><a href="#frase" class="acr"><span></span>Scroll</a></div>
     </header>
+
+    <div class="frase d-flex align-items-center" id="frase">
+      <div class="container text-center">
+        <h1 class="text-uppercase text-white">EU ACREDITO EM UM BRASIL MELHOR COM <span class="text-info">ATAÍDE TERUEL</span></h1>
+      </div>
+    </div>
+
+    <div class="causa">
+    	<div class="container">
+        <div class="row">
+        	<div class="col-md-10 col-sm-12 offset-md-1">
+		        <h3 class="text-center"><span class="tt bg-warning p-2">MINHA CAUSA É POR AMOR AO BRASIL</span></h3>
+		        <ol>
+		        	<li><b class="text-uppercase">Lorem ipsum dolor sit amet:</b> consectetur adipisicing elit. Saepe ipsa qui error quos id quod architecto perferendis libero voluptate ipsam quisquam, in dolores velit natus ullam, cumque, repudiandae blanditiis deserunt.</li>
+		        	<li><b class="text-uppercase">Lorem ipsum dolor sit amet:</b> consectetur adipisicing elit. Saepe ipsa qui error quos id quod architecto perferendis libero voluptate ipsam quisquam, in dolores velit natus ullam, cumque, repudiandae blanditiis deserunt.</li>
+		        	<li><b class="text-uppercase">Lorem ipsum dolor sit amet:</b> consectetur adipisicing elit. Saepe ipsa qui error quos id quod architecto perferendis libero voluptate ipsam quisquam, in dolores velit natus ullam, cumque, repudiandae blanditiis deserunt.</li>
+		        	<li><b class="text-uppercase">Lorem ipsum dolor sit amet:</b> consectetur adipisicing elit. Saepe ipsa qui error quos id quod architecto perferendis libero voluptate ipsam quisquam, in dolores velit natus ullam, cumque, repudiandae blanditiis deserunt.</li>
+		        	<li><b class="text-uppercase">Lorem ipsum dolor sit amet:</b> consectetur adipisicing elit. Saepe ipsa qui error quos id quod architecto perferendis libero voluptate ipsam quisquam, in dolores velit natus ullam, cumque, repudiandae blanditiis deserunt.</li>
+		        </ol>
+		      </div>
+		    </div>
+      </div>
+    </div>
+    
+    <div class="frase-2-wp">
+	    <div class="frase-2">
+	      <div class="container text-center">
+	        <h2 class="text-uppercase text-danger">
+						ACEITE SEU PASSADO SEM ARREPENDIMENTOS, LIDE COM SEU PRESENTE COM CONFIANÇA E ENFRENTE SEU FUTURO SEM MEDO.
+	        </h2>
+	      </div>
+	    </div>
+    </div>
+
+   <div class="ajude text-center">
+    	<div class="container">
+        <div class="row">
+        	<div class="col-md-10 col-sm-12 offset-md-1">
+	    			<h3>AJUDE A COMPARTILHAR A VERDADE</h3>
+		        <div class="row">
+		        	<div class="col-md-4">
+		        		<span class="img-bg bg-info" style="padding-top:35px;"><img src="images/icon-facebook.png" alt=""></span>
+		        		<p><h5>ACOMPANHA<br>FACEBOOK</h5></p>
+		        	</div>
+		        	<div class="col-md-4">
+		        		<span class="img-bg bg-warning"><img src="images/icon-monitor.png" alt=""></span>
+		        		<p><h5>REPOSITÓRIO<br>DIGITAL</h5></p>
+		        	</div>
+		        	<div class="col-md-4">
+		        		<span class="img-bg bg-dark"><img src="images/icon-question.png" alt=""></span>
+		        		<p><h5>COMO AJUDAR</h5></p>
+		        	</div>
+		        </div>
+					</div>
+				</div>
+      </div>
+    </div>
+		
+		<footer class="d-flex align-items-center bg-dark text-white">
+      <div class="container text-center">
+        <div class="row">
+        	<div class="col-md-10 col-sm-12 offset-md-1">
+		      	<h4 class="text-uppercase">Convide 10 amigos para apoiar</h4>
+		      	<a href="#" class="btn btn-success mt-5">CONVIDAR</a>
+		      	<hr class="mt-5">
+		      	<p class="text-secondary mt-5">© ataideteruel.com.br | PORQUE SEU DIA MERECE SER FELIZ.</p>
+		      </div>
+	      </div>
+      </div>
+		</footer>
+
+
+
+
 
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
   </body>
 </html>
