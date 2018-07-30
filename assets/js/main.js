ad66714ccb0e749a349b53e3e636a9b4286a228f
Igor dos Santos <igor.santos@fiap.com.br>
2018-07-30 19:26:58 -0300

    dev

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 800, 'linear');
   });
 
+  // Fixa menu
+	$(window).scroll(function() {
+		var windscroll = $(window).scrollTop();
+		if (windscroll >= 496) {
+			$('nav.mn-desktop').addClass('fixed');
+			$('body').css('padding-top','142px');
+		} else {
+			$('nav.mn-desktop').removeClass('fixed');
+			$('body').css('padding-top','0');
+		}
+	}).scroll();
+
 });
